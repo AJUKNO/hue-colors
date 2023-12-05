f4fda6f09fcf85ed8106b8897206a1070cdd604a
Aj Sijpenhof <aj.sijpenhof@hva.nl>
2023-12-05 14:29:02 +0100

    feat(helper): added helper function for brush gradient

---
+++ app/src/main/java/nl/hva/huecolors/utils/Utils.kt
@@ -0,0 +1,17 @@
+package nl.hva.huecolors.utils
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+class Utils {
+
+    companion object {
+        fun gradient(first: Color, second: Color): Brush = Brush.horizontalGradient(
+            listOf(
+                first,
+                second
+            )
+        )
+    }
+}
