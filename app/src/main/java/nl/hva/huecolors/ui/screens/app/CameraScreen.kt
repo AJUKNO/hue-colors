d201d865a75862727f866876786e92475f243995
Aj Sijpenhof <aj.sijpenhof@hva.nl>
2024-01-12 05:18:14 +0100

    feat(screens): added loading state to camera palette drawer

--- app/src/main/java/nl/hva/huecolors/ui/screens/app/CameraScreen.kt
+++ app/src/main/java/nl/hva/huecolors/ui/screens/app/CameraScreen.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Build
-import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -21,9 +20,11 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -31,19 +32,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -58,25 +54,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
 import androidx.palette.graphics.Palette
-import coil.compose.AsyncImage
-import coil.request.CachePolicy
-import coil.request.ImageRequest
-import coil.size.Scale
 import kotlinx.coroutines.launch
 import nl.hva.huecolors.R
 import nl.hva.huecolors.ui.components.HueButton
-import nl.hva.huecolors.ui.screens.Screens
 import nl.hva.huecolors.utils.Utils
 import nl.hva.huecolors.viewmodel.CameraViewModel
 import nl.hva.huecolors.viewmodel.LightViewModel
@@ -84,7 +74,11 @@ import nl.hva.huecolors.viewmodel.LightViewModel
 @RequiresApi(Build.VERSION_CODES.S)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
+fun CameraScreen(
+    navController: NavHostController,
+    viewModel: CameraViewModel,
+    padding: PaddingValues
+) {
     val coroutineScope = rememberCoroutineScope()
     val permissions = arrayOf(
         Manifest.permission.CAMERA
@@ -93,6 +87,7 @@ fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
+    var loadingState by remember { mutableStateOf<Pair<Boolean, Boolean>>(false to false) }
     val cameraController = remember { LifecycleCameraController(context) }
     val capturedImage by viewModel.capturedImage.observeAsState()
     var showBottomSheet by rememberSaveable {
@@ -105,9 +100,6 @@ fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissionsMap ->
         val areGranted = permissionsMap.values.all { it }
-        if (!areGranted) {
-            // TODO: HANDLE PERMISSION DENIAL
-        }
     }
     val isGranted = Utils.checkPermissions(context, permissions)
 
@@ -125,58 +117,63 @@ fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
         }
     }
 
-
     Crossfade(
         targetState = isGranted,
         label = "",
         modifier = Modifier.fillMaxSize()
     ) { granted ->
         if (granted) {
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                floatingActionButtonPosition = FabPosition.Center,
-                floatingActionButton = {
-                    FloatingActionButton(
-                        onClick = {
-                            val mainExecutor = ContextCompat.getMainExecutor(context)
-                            cameraController.takePicture(mainExecutor,
-                                object : ImageCapture.OnImageCapturedCallback() {
-                                    override fun onCaptureSuccess(image: ImageProxy) {
-                                        viewModel.captureImage(image.toBitmap())
-                                        showBottomSheet = true
-                                    }
-                                })
-                        },
-                        modifier = Modifier
-                            .size(124.dp)
-                            .padding(24.dp),
-                        shape = CircleShape,
-                        containerColor = MaterialTheme.colorScheme.primary
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_camera),
-                            contentDescription = "Camera",
-                            modifier = Modifier.size(28.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        start = padding.calculateStartPadding(LayoutDirection.Ltr),
+                        end = padding.calculateEndPadding(LayoutDirection.Ltr),
+                        bottom = padding.calculateBottomPadding()
+                    )
+            ) {
+
+                AndroidView(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(0.dp), factory = { ctx ->
+                    PreviewView(ctx).apply {
+                        layoutParams = LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
                         )
+                        scaleType = PreviewView.ScaleType.FILL_START
+                    }.also { previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
                     }
-                }) { padding ->
-                Box(modifier = Modifier.fillMaxSize()) {
-                    AndroidView(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(padding), factory = { ctx ->
-                        PreviewView(ctx).apply {
-                            layoutParams = LinearLayout.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.MATCH_PARENT
-                            )
-                            scaleType = PreviewView.ScaleType.FILL_START
-                        }.also { previewView ->
-                            previewView.controller = cameraController
-                            cameraController.bindToLifecycle(lifecycleOwner)
-                        }
-                    })
+                })
+
+                FloatingActionButton(
+                    onClick = {
+                        val mainExecutor = ContextCompat.getMainExecutor(context)
+                        cameraController.takePicture(mainExecutor,
+                            object : ImageCapture.OnImageCapturedCallback() {
+                                override fun onCaptureSuccess(image: ImageProxy) {
+                                    viewModel.captureImage(image.toBitmap())
+                                    showBottomSheet = true
+                                }
+                            })
+                    },
+                    modifier = Modifier
+                        .size(124.dp)
+                        .padding(24.dp)
+                        .align(Alignment.BottomCenter),
+                    shape = CircleShape,
+                    containerColor = MaterialTheme.colorScheme.primary
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_camera),
+                        contentDescription = "Camera",
+                        modifier = Modifier.size(28.dp)
+                    )
                 }
             }
+
 
             CameraDrawer(
                 onDismiss = {
@@ -185,22 +182,27 @@ fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
                 image = capturedImage,
                 onApply = { palette ->
                     coroutineScope.launch {
+                        loadingState = loadingState.copy(false, true)
                         lightViewModel.paletteToLights(palette)
                         sheetState.hide()
                     }.invokeOnCompletion {
                         if (!sheetState.isVisible) {
                             showBottomSheet = false
+                            loadingState = loadingState.copy(false, false)
                         }
                     }
                 },
+                loadingState = loadingState,
                 onSaveApply = { palette ->
                     coroutineScope.launch {
+                        loadingState = loadingState.copy(true)
                         lightViewModel.paletteToLights(palette)
                         viewModel.saveToStorage(capturedImage, context)
                         sheetState.hide()
                     }.invokeOnCompletion {
                         if (!sheetState.isVisible) {
                             showBottomSheet = false
+                            loadingState = loadingState.copy(false)
                         }
                     }
                 },
@@ -210,7 +212,9 @@ fun CameraScreen(navController: NavHostController, viewModel: CameraViewModel) {
             )
         } else {
             Column(
-                modifier = Modifier.fillMaxSize().padding(24.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(24.dp),
                 verticalArrangement = Arrangement.Center
             ) {
                 HueButton(text = "Allow camera permission", onClick = {
@@ -232,8 +236,10 @@ fun CameraDrawer(
     onSaveApply: (Palette?) -> Unit,
     isVisible: Boolean,
     sheetState: SheetState,
-    context: Context
+    context: Context,
+    loadingState: Pair<Boolean, Boolean>
 ) {
+    val (saveApply, apply) = loadingState
     val palette = image?.let { Utils.getPalette(it, 6) }
     if (isVisible) {
         ModalBottomSheet(
@@ -291,13 +297,21 @@ fun CameraDrawer(
 
                 Spacer(modifier = Modifier.size(48.dp))
 
-                HueButton(text = "Save and Apply",
+                HueButton(
+                    text = "Save and Apply",
+                    onClick = { onSaveApply(palette) },
                     secondary = true,
-                    onClick = { onSaveApply(palette) })
+                    loading = saveApply
+                )
 
                 Spacer(modifier = Modifier.size(8.dp))
 
-                HueButton(text = "Apply", onClick = { onApply(palette) })
+                HueButton(
+                    text = "Apply",
+                    onClick = { onApply(palette) },
+                    loading = apply,
+                    secondary = true
+                )
             }
         }
     }
